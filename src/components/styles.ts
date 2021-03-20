aa5d82ff6c4522f3b7f4e5ac05e9bce22cfc14af
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-03-20 23:14:05 +0100

    feat: Added helpers function

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,17 +1,39 @@
 import {StyleSheet} from 'react-native';
-import {wp} from '../utils/layout';
+import {font} from '../constants/font';
+import {hp, wp} from '../utils/layout';
 
 export const GameStyles = StyleSheet.create({
-  container: {
-    // alignItems: 'center',
+  vs: {
+    ...font[1],
+    lineHeight: hp(55),
+    fontSize: hp(50),
+    marginHorizontal: wp(4),
+    letterSpacing: -wp(5),
+  },
+  textContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {},
+  player: {
+    alignSelf: 'center',
+    fontSize: hp(35),
+    fontFamily: 'YanoneKaffeesatz-Regular',
+    textTransform: 'lowercase',
   },
 });
 
 export const SquareStyles = StyleSheet.create({
+  text: {
+    fontSize: hp(50),
+  },
   container: {
     borderWidth: 1,
     width: wp(100),
     height: wp(100),
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
