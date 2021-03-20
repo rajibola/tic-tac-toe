1394d042d69a342ec738b1a1328641590acb0f64
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-03-20 23:13:28 +0100

    feat: Added constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  white: '#fff',
+  dark: '#353b48',
+  black: '#2f3640',
+  blue: '#192a56',
+};
---
+++ src/constants/font.ts
@@ -0,0 +1,17 @@
+import {hp} from '../utils/layout';
+import {colors} from './colors';
+
+export const font = {
+  1: {
+    fontFamily: 'Redressed-Regular',
+    fontSize: hp(16),
+    lineHeight: hp(20),
+    color: colors.black,
+  },
+  2: {
+    fontFamily: 'YanoneKaffeesatz-Regular',
+    fontSize: hp(16),
+    lineHeight: hp(20),
+    color: colors.black,
+  },
+};
